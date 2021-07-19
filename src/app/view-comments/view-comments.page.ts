e15a798f7f5fada267d3e08ec48f54e44b374c0d
Anton Pogrebnyi <anton.pogrebnyi@hotmail.com>
2021-07-19 11:08:16 +1000

    Time correction

--- src/app/view-comments/view-comments.page.ts
+++ src/app/view-comments/view-comments.page.ts
@@ -11,17 +11,33 @@ import { DataService, Message, Comment } from '../services/data.service';
 export class ViewCommentsPage implements OnInit {
   public post: Message;
   comments: Array<Comment>;
+  time: string;
 
   constructor(
     private data: DataService,
     private activatedRoute: ActivatedRoute,
     private http: HttpClient
-  ) { }
+  ) {
+    console.log(this.comments);
+
+   }
 
   ngOnInit() {
     const id = this.activatedRoute.snapshot.paramMap.get('id');
     this.getPosts(id);
     this.post = this.data.getMessageById(parseInt(id, 10));
+    this.time = this.formatAMPM(new Date);
+  }
+
+  formatAMPM(date) {
+    var hours = date.getHours();
+    var minutes = date.getMinutes();
+    var ampm = hours >= 12 ? 'PM' : 'AM';
+    hours = hours % 12;
+    hours = hours ? hours : 12; // the hour '0' should be '12'
+    minutes = minutes < 10 ? '0'+minutes : minutes;
+    var strTime = hours + ':' + minutes + ' ' + ampm;
+    return strTime;
   }
 
   getBackButtonText() {
